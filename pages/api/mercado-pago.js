95792066688a39b70b103bb4e276adec03dde57e
nikysalama <nikysalama0@example.com>
2024-12-27 20:03:07 -0300

    fix: updated form of connection to mercado pago

--- pages/api/mercado-pago.js
+++ pages/api/mercado-pago.js
@@ -1,30 +1,44 @@
-const mercadopago = require('mercadopago');
+import { MercadoPagoConfig, Preference } from 'mercadopago';
 
 // Configura el SDK con tu Access Token
-mercadopago.configure({
-  access_token: 'APP_USR-1406847872002743-121821-250086ca1c29da9c3dfc6b28e387745f-1365029203',
-});
+const client = new MercadoPagoConfig({ accessToken: 'APP_USR-1406847872002743-121821-250086ca1c29da9c3dfc6b28e387745f-1365029203' });
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     try {
-      const preference = {
-        items: [
-          {
-            title: req.body.title,
-            unit_price: req.body.unit_price,
-            quantity: req.body.quantity,
-          },
-        ],
-        back_urls: {
-          success: 'https://davora.vercel.app',
-          failure: 'https://facebook.com',
-          pending: 'https://instagram.com',
-        },
-        auto_return: 'approved',
-      };
+      // Crear la preferencia usando la nueva API
+      const preference = new Preference(client);
 
-      const response = await mercadopago.preferences.create(preference);
+      const response = await preference.create({
+        body: {
+          items: [
+            {
+              title: req.body.title,
+              quantity: req.body.quantity,
+              unit_price: req.body.unit_price,
+            },
+          ],
+          payment_methods: {
+            excluded_payment_methods: [
+              { id: "argencard" },
+              { id: "cmr" },
+              { id: "cencosud" },
+              { id: "diners" },
+              { id: "tarshop" },
+              { id: "maestro" }
+            ],
+            installments: 1,
+          },
+          back_urls: {
+            success: 'https://davora.vercel.app/success',
+            failure: 'https://davora.vercel.app/failure',
+            pending: 'https://davora.vercel.app/pending',
+          },
+          auto_return: 'approved',
+        }
+      });
+
+      // Retorna la URL de pago generada
       res.status(200).json({ init_point: response.body.init_point });
     } catch (error) {
       console.error('Error creando la preferencia:', error);
